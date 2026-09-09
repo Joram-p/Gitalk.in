831594cb22ed79ee6e25c857792d82ee90486cc2
Joram-p <141070371+Joram-p@users.noreply.github.com>
2026-09-09 16:45:48 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -20,16 +20,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-
 const db = getFirestore(app);
-
 const storage = getStorage(app);
 
-export {
-  app,
-  auth,
-  db,
-  storage
-};
+export { app, auth, db, storage };
