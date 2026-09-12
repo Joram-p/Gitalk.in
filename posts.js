6e6902228a0e55ea5a0af8e8800d9de4ddd53383
Joram-p <141070371+Joram-p@users.noreply.github.com>
2026-09-12 15:18:46 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,10 @@
+{
+  title:"Aapka New Post",
+  category:"Nature",
+  description:"Yahan apne post ki short information likho.",
+  date:"2026-09-12",
+  author:"JORAM",
+  icon:"🌿",
+  tags:["nature","plants","flowers"],
+  link:"#"
+},
